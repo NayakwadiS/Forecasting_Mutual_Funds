f5dc6688ebd97e4d2e9b77f58acd0d0b73f69a87
Void_User <39816455+NayakwadiS@users.noreply.github.com>
2022-08-29 14:05:54 +0530

    v2.5
    move to Seaborn from matplotlib

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
 from Algorithms import *
 from tabulate import tabulate
 
@@ -5,8 +8,7 @@ scheme_code = input('Enter the MF Scheme code:- ')
 
 
 @getDataFrame(scheme_code)
-def forecasting_mutual_fund(df,details):
-
+def forecasting_mutual_fund(df, details):
     # pred_SMA = SMA(df)
     pred_linear, rmse_linear = linear(df)
     pred_autoReg, rmse_auto = AutoR(df)
@@ -14,32 +16,41 @@ def forecasting_mutual_fund(df,details):
     pred_LSTM, rmse_lstm = lstm(df)
 
     data = [
-            ['Linear', pred_linear[0], pred_linear[1],pred_linear[2],pred_linear[3],pred_linear[4]
-                ,min(pred_linear),max(pred_linear)],
-            ['Auto Regression',pred_autoReg[0], pred_autoReg[1],pred_autoReg[2],pred_autoReg[3]
-                ,pred_autoReg[4],min(pred_autoReg),max(pred_autoReg)],
-            ['ARIMA', pred_arima[0], pred_arima[1], pred_arima[2], pred_arima[3], pred_arima[4],
-             min(pred_arima), max(pred_arima)],
-            ['LSTM', pred_LSTM[0], pred_LSTM[1],pred_LSTM[2],pred_LSTM[3],pred_LSTM[4]
-                ,min(pred_LSTM),max(pred_LSTM)]
-            ]
-    print("\n  Time Series Forecasting for "+details['scheme_name']+" ("+str(details['scheme_code'])+")\n")
-    print(tabulate(data, headers=["Algorithm", "Day 1", "Day 2", "Day 3","Day 4","Day 5",
-                                  "1 Month Low","1 Month High"], tablefmt='orgtbl'))
+        ['Linear', pred_linear[0], pred_linear[1], pred_linear[2], pred_linear[3], pred_linear[4]
+            , min(pred_linear), max(pred_linear)],
+        ['Auto Regression',pred_autoReg[0], pred_autoReg[1],pred_autoReg[2],pred_autoReg[3]
+            ,pred_autoReg[4],min(pred_autoReg),max(pred_autoReg)],
+        ['ARIMA', pred_arima[0], pred_arima[1], pred_arima[2], pred_arima[3], pred_arima[4],
+         min(pred_arima), max(pred_arima)],
+        ['LSTM', pred_LSTM[0], pred_LSTM[1],pred_LSTM[2],pred_LSTM[3],pred_LSTM[4]
+            ,min(pred_LSTM),max(pred_LSTM)]
+    ]
+    print("\n  Time Series Forecasting for " + details['scheme_name'] + " (" + str(details['scheme_code']) + ")\n")
+    print(tabulate(data, headers=["Algorithm", "Day 1", "Day 2", "Day 3", "Day 4", "Day 5",
+                                  "1 Month Low", "1 Month High"], tablefmt='orgtbl'))
 
     df_30 = df['nav'].iloc[-100:].astype(float)
     Y = [np.nan for i in range(len(df_30))]
 
-    plt.plot(df_30.values,color='black',label='Trend')                      # last 100 days
-    plt.plot(np.append(Y,pred_linear), color='green',label="Linear")
-    plt.plot(np.append(Y, pred_autoReg), color='blue', label="Auto Regression")
-    plt.plot(np.append(Y,pred_arima), color='red',label='ARIMA')
-    plt.plot(np.append(Y, pred_LSTM), color='orange', label='LSTM')
+    # plt.plot(df_30.values, color='black', label='Trend')  # last 100 days
+    # plt.plot(np.append(Y, pred_linear), color='green', label="Linear")
+    # plt.plot(np.append(Y, pred_autoReg), color='blue', label="Auto Regression")
+    # plt.plot(np.append(Y,pred_arima), color='red',label='ARIMA')
+    # plt.plot(np.append(Y, pred_LSTM), color='orange', label='LSTM')
     plt.xlabel('Days [last 100 + 30 forecasted]')
     plt.ylabel('NAV')
-    plt.title("Forecasting for "+details['scheme_name'])
+    plt.title("Forecasting for " + details['scheme_name'])
     plt.legend()
+    s = df_30.append(pd.Series([np.nan for i in range(30)]))
+    sns.set(style="ticks")
+    data_preproc = pd.DataFrame({
+        'Trends': s.values,
+        'Linear': np.append(Y, pred_linear),
+        'Auto Regression': np.append(Y, pred_autoReg),
+        'ARIMA': np.append(Y, pred_arima),
+        'LSTM': np.append(Y, pred_LSTM),
+    })
+    sns.lineplot(data=data_preproc)
     plt.show()
-
 
 forecasting_mutual_fund()
